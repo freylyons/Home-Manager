0c8dd88a2689440c7a78f752b70b4bedde2cb563
frey <freylyons@hotmail.co.uk>
2026-01-24 14:23:49 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Home Manager Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+
+    homeConfigurations."frey" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
+    };
+  };
+}

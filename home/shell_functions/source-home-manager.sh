8de5f7af23ec10124083e13ef3c0516e5a81b110
frey <freylyons@hotmail.co.uk>
2026-04-19 17:27:30 +0100

    added resourcing the bashrc to the source-home-manager shell function

--- home/shell_functions/source-home-manager.sh
+++ home/shell_functions/source-home-manager.sh
@@ -2,5 +2,6 @@
 source-home-manager() {
   unset __HM_SESS_VARS_SOURCED
   source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+  source "$HOME/.bashrc"
   echo "Home Manager environment variables reloaded."
 }
